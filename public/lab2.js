4cdd17c1c8a58518cd15832a9cded47a7c065b9f
LNorbert1 <norbertludasz@gmail.com>
2023-04-14 21:04:45 +0300

    i really love eslint

--- public/lab2.js
+++ public/lab2.js
@@ -24,80 +24,101 @@ for (let i = 0; i < 4; i++) {
 
 let currentRow = 0;
 
-function fillNextRow() {
+const colorform = document.getElementById('colorform');
+
+colorform.addEventListener('submit', (event) => {
   event.preventDefault();
-  // console.log('entered fillnextrow');
+});
+
+function fillTable(displayedColors) {
+  for (let currentColumn = 0; currentColumn < 4; currentColumn++) {
+    context.fillStyle = displayedColors[currentColumn];
+    context.fillRect(currentColumn * 50, currentRow * 50, 50, 50);
+  }
+}
+
+function countCorrect() {
   const color1 = document.getElementById('color1').value;
   const color2 = document.getElementById('color2').value;
   const color3 = document.getElementById('color3').value;
   const color4 = document.getElementById('color4').value;
-  // console.log(color1);
 
   let correctBoth = 0;
   let correctColor = 0;
 
   if (generatedColors.includes(color1)) {
-    if (generatedColors[0] == color1) {
-      correctBoth = correctBoth + 1;
+    if (generatedColors[0] === color1) {
+      correctBoth += 1;
     } else {
-      correctColor = correctColor + 1;
+      correctColor += 1;
     }
   }
 
   if (generatedColors.includes(color2)) {
-    if (generatedColors[1] == color2) {
-      correctBoth = correctBoth + 1;
+    if (generatedColors[1] === color2) {
+      correctBoth += 1;
     } else {
-      correctColor = correctColor + 1;
+      correctColor += 1;
     }
   }
 
   if (generatedColors.includes(color3)) {
-    if (generatedColors[2] == color3) {
-      correctBoth = correctBoth + 1;
+    if (generatedColors[2] === color3) {
+      correctBoth += 1;
     } else {
-      correctColor = correctColor + 1;
+      correctColor += 1;
     }
   }
 
   if (generatedColors.includes(color4)) {
-    if (generatedColors[3] == color4) {
-      correctBoth = correctBoth + 1;
+    if (generatedColors[3] === color4) {
+      correctBoth += 1;
     } else {
-      correctColor = correctColor + 1;
+      correctColor += 1;
     }
   }
 
-  displayedColors = [];
-  displayedLen = 0;
+  console.log(correctBoth);
+  console.log(correctColor);
+  return [correctBoth, correctColor];
+}
+
+// eslint-disable-next-line
+function fillNextRow() {
+  // console.log('entered fillnextrow');
+
+  // console.log(color1);
+
+  let [correctBoth, correctColor] = countCorrect(generatedColors);
+  console.log(correctBoth, correctColor);
+
+  const displayedColors = [];
+  let displayedLen = 0;
 
   while (correctBoth > 0) {
-    correctBoth = correctBoth - 1;
+    correctBoth -= 1;
     displayedColors[displayedLen] = 'white';
-    displayedLen = displayedLen + 1;
+    displayedLen += 1;
   }
 
   while (correctColor > 0) {
-    correctColor = correctColor - 1;
+    correctColor -= 1;
     displayedColors[displayedLen] = 'black';
-    displayedLen = displayedLen + 1;
+    displayedLen += 1;
   }
 
   while (displayedLen < 4) {
     displayedColors[displayedLen] = 'red';
-    displayedLen = displayedLen + 1;
+    displayedLen += 1;
   }
 
   // console.log(displayedColors);
 
-  for (let currentColumn = 0; currentColumn < 4; currentColumn++) {
-    context.fillStyle = displayedColors[currentColumn];
-    context.fillRect(currentColumn * 50, currentRow * 50, 50, 50);
-  }
+  fillTable(displayedColors);
 
   currentRow++;
 
-  if (displayedColors[3] == 'white') {
+  if (displayedColors[3] === 'white') {
     // console.log("You win!");
     const pWin = document.getElementById('youwin');
     pWin.style.display = 'block';
