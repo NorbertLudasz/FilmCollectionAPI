38256a896decf4871ecceed1900b370f33303fce
LNorbert1 <norbertludasz@gmail.com>
2023-06-11 08:34:55 +0300

    error messages

--- public/public/ajax.js
+++ public/public/ajax.js
@@ -5,11 +5,16 @@ async function getMessage(filmID) {
   try {
     console.log('getmessage called');
     const fetchresult = await fetch(`/message/${filmID}`);
-    const jsonresult = await fetchresult.json();
-    console.log(jsonresult);
-    document.getElementById('elrejtett').innerText = `Zsaner: ${jsonresult.zsaner}, Leiras: ${jsonresult.leiras}`;
+    if (fetchresult.ok) {
+      const jsonresult = await fetchresult.json();
+      console.log(jsonresult);
+      document.getElementById('elrejtett').innerText = `Zsaner: ${jsonresult.zsaner}, Leiras: ${jsonresult.leiras}`;
+    } else {
+      document.getElementById('elrejtett').innerText = 'Error: response from fetching message/filmID was not ok';
+    }
   } catch (err) {
     console.log(err);
+    document.getElementById('elrejtett').innerText = `Error: ${err.message}`;
   }
 }
 
@@ -23,9 +28,13 @@ async function reviewdel(reviewid) {
     console.log(reviewid);
     if (response.ok) {
       document.getElementById(`review${reviewid}`).remove();
+    } else {
+      document.getElementById('delreviewmessage').innerText =
+        'Error: response from fetching reviewDel/reviewid was not ok';
     }
   } catch (err) {
     // ird ki hogy hiba volt kepernyore is
     console.log(err);
+    document.getElementById('delreviewmessage').innerText = `Error: ${err.message}`;
   }
 }
